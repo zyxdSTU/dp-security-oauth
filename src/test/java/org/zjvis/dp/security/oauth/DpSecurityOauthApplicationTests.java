81c0afa1c7e7f0d34f6cf14db37b665093637d8f
zhouyu <zhouyu@zhejianglab.com>
2023-07-21 15:03:07 +0800

    first init

---
+++ src/test/java/org/zjvis/dp/security/oauth/DpSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zjvis.dp.security.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSecurityOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
